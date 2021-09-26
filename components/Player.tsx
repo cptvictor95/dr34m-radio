52ffda7226a1e925676ae5d70d1a7fea285641a8
cptvictor95 <cpt.victor@hotmail.com>
2021-09-26 17:44:56 -0300

    add playerProps to audio player

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Player: React.FC<{ autoPlay: boolean; controls: boolean; src: string }> =
-  ({ autoPlay, controls, src }) => {
-    return (
-      <div>
-        <audio autoPlay={autoPlay} controls={controls} src={src}></audio>
-      </div>
-    );
-  };
+type PlayerProps = React.DetailedHTMLProps<
+  React.AudioHTMLAttributes<HTMLAudioElement>,
+  HTMLAudioElement
+> & {
+  autoPlay: boolean;
+  controls: boolean;
+  src: string;
+};
+
+const Player: React.FC<PlayerProps> = ({ autoPlay, controls, src }) => {
+  return (
+    <div>
+      <audio autoPlay={autoPlay} controls={controls} src={src}></audio>
+    </div>
+  );
+};
 
 export default Player;
